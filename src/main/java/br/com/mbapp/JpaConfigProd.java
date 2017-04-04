0543cc1dd63eb0821a8d6a121ea32b98f0c59e6c
Michelli <michellidibrito@gmail.com>
2017-04-04 13:38:12 -0300

    procfile

--- src/main/java/br/com/mbapp/JpaConfigProd.java
+++ src/main/java/br/com/mbapp/JpaConfigProd.java
@@ -11,30 +11,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-@Profile("prod")
-public class JpaConfigProd {
-
-	 @Bean
-	    public DataSource dataSource()throws URISyntaxException{
-	        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	        dataSource.setDriverClassName("org.postgresql.Driver");
-
-	        URI dbUrl = new URI(Environment.DATASOURCE);
-
-
-	        dataSource.setUrl("jdbc:postgresql://"+dbUrl.getHost()+":"+dbUrl.getPort()+dbUrl.getPath());
-	        dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
-	        dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
-
-	        return dataSource;
-	    }
-
-	    @Bean
-	    private Properties aditionalProperties(){
-	        Properties props = new Properties();
-	        props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-	        props.setProperty("hibernate.show_sql", "false");
-	        props.setProperty("hibernate.hbm2ddl.auto", "update");
-	        return props;
-	    }
-}
+//@Profile("prod")
+//public class JpaConfigProd {
+//
+//	 @Bean
+//	    public DataSource dataSource()throws URISyntaxException{
+//	        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//	        dataSource.setDriverClassName("org.postgresql.Driver");
+//
+//	        URI dbUrl = new URI(Environment.DATASOURCE);
+//
+//
+//	        dataSource.setUrl("jdbc:postgresql://"+dbUrl.getHost()+":"+dbUrl.getPort()+dbUrl.getPath());
+//	        dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
+//	        dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
+//
+//	        return dataSource;
+//	    }
+//
+//	    @Bean
+//	    private Properties aditionalProperties(){
+//	        Properties props = new Properties();
+//	        props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+//	        props.setProperty("hibernate.show_sql", "false");
+//	        props.setProperty("hibernate.hbm2ddl.auto", "update");
+//	        return props;
+//	    }
+//}
