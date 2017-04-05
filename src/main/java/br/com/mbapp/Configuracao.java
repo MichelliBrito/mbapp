0d7bc654f2275aff3a6fcba7137498c8086b57d9
Michelli <michellidibrito@gmail.com>
2017-04-05 16:07:15 -0300

    start

--- src/main/java/br/com/mbapp/Configuracao.java
+++ src/main/java/br/com/mbapp/Configuracao.java
@@ -11,8 +11,6 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -27,19 +25,18 @@ public class Configuracao extends WebMvcConfigurerAdapter{
         SpringApplication.run(Configuracao.class, args);
     }
 	
-	@Bean
-	@Profile("local")
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/mbappweb");
-        dataSource.setUsername("root");
-        dataSource.setPassword("Michelli14");
-        return dataSource;
-    }
+//	@Bean
+//	@Profile("local")
+//    public DataSource dataSource(){
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/mbappweb");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("Michelli14");
+//        return dataSource;
+//    }
 	
 	@Bean
-	@Profile("prod")
     public DataSource dataSourceProd() throws URISyntaxException{
         
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
