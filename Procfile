839e7484b22c1d88fcbb99d63b0e5092dbf325ed
Michelli <michellidibrito@gmail.com>
2017-04-05 15:36:28 -0300

    pronto para deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -jar -Dspring.profiles.active=prod target/dependency/webapp-runner.jar --port $PORT target/*.war
+web: java $JAVA_OPTS -Dspring.profiles.active=prod -jar target/dependency/webapp-runner.jar --port $PORT target/*.war
--- src/main/java/br/com/mbapp/Configuracao.java
+++ src/main/java/br/com/mbapp/Configuracao.java
@@ -1,9 +1,13 @@
 package br.com.mbapp;
 
-import java.util.Properties;
+
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -23,45 +27,44 @@ public class Configuracao extends WebMvcConfigurerAdapter{
         SpringApplication.run(Configuracao.class, args);
     }
 	
-//	@Bean
-//    public DataSource dataSource(){
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-//        dataSource.setUrl("jdbc:mysql://localhost:3306/mbappweb");
-//        dataSource.setUsername("root");
-//        dataSource.setPassword("Michelli14");
-//        return dataSource;
-//    }
-//	
-//	@Bean
-//	@Profile("dev")
-//	public JpaVendorAdapter jpaVendorAdapter(){//cria um bean Hibernate
-//		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-//		adapter.setDatabase(Database.MYSQL);
-//		adapter.setShowSql(false);//mostrar codigo sql no console.
-//		adapter.setGenerateDdl(true);//habilita para que o hibernate crie as tabelas automaticamente, pois já estamos usando o flyway para isso.
-//		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
-//		return adapter;
-//	}
+	@Bean
+	@Profile("local")
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mbappweb");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Michelli14");
+        return dataSource;
+    }
 	
 	@Bean
 	@Profile("prod")
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgres://ec2-54-235-123-159.compute-1.amazonaws.com:5432/d4rir18f73tdnm");
-        dataSource.setUsername("zvnvotqtguaxkw");
-        dataSource.setPassword("9ee3a230201c5f01bc0edfea50f6bcb7ee727f24d499cf2522006d7aeb43bbf2");
+    public DataSource dataSourceProd() throws URISyntaxException{
+        
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setInitialSize(20);
+        
         return dataSource;
     }
-
-    @Bean
-    private Properties aditionalProperties(){
-        Properties props = new Properties();
-        props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        props.setProperty("hibernate.show_sql", "false");
-        props.setProperty("hibernate.hbm2ddl.auto", "update");
-        return props;
-    }
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter(){//cria um bean Hibernate
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(false);//mostrar codigo sql no console.
+		adapter.setGenerateDdl(true);//habilita para que o hibernate crie as tabelas automaticamente, pois já estamos usando o flyway para isso.
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		return adapter;
+	}
 	
 }
